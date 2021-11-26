3fb8f7898ba616e797be1e7a489029d4178139d8
SaigyoujiNono <1029259127@qq.com>
2021-11-26 14:51:10 +0800

    完成首页数据显示

---
+++ service/service_edu/src/main/java/com/mqd/eduservice/controller/front/HomeController.java
@@ -0,0 +1,52 @@
+package com.mqd.eduservice.controller.front;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.mqd.eduservice.pojo.EduCourse;
+import com.mqd.eduservice.pojo.EduTeacher;
+import com.mqd.eduservice.service.EduCourseService;
+import com.mqd.eduservice.service.EduTeacherService;
+import com.mqd.result.Result;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * 首页数据展示
+ */
+@RestController
+@RequestMapping("/eduservice/home")
+@CrossOrigin
+public class HomeController {
+    @Resource
+    private EduTeacherService teacherService;
+
+    @Resource
+    private EduCourseService courseService;
+
+    @ApiOperation(value="获取9条热门课程数据和6条讲师数据",notes = "获取9条根据观看量排序热门课程数据，6条根据sort字段排序的讲师数据")
+    @GetMapping("/home")
+    public Result getHomeData(){
+        // 获取9条课程数据
+        QueryWrapper<EduCourse> courseQuery = new QueryWrapper<>();
+        IPage<EduCourse> coursePage = new Page<>(1,9);
+        courseQuery.eq("status","Normal")   //获取已发布的
+                .orderByDesc("view_count");
+        courseService.page(coursePage, courseQuery);
+        //获取6条讲师数据
+        QueryWrapper<EduTeacher> teacherQuery = new QueryWrapper<>();
+        IPage<EduTeacher> teacherPage = new Page<>(1,6);
+        courseQuery.orderByDesc("sort");
+        teacherService.page(teacherPage, teacherQuery);
+        //最终返回
+        return Result.ok().addData("hotCourse",coursePage.getRecords())
+                .addData("hotTeacher",teacherPage.getRecords());
+    }
+
+
+}
