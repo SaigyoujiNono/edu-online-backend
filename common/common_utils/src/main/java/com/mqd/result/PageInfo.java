8038fd1421275970c0b624c62326b951b03a5085
SaigyoujiNono <1029259127@qq.com>
2021-12-15 15:51:00 +0800

    课程分类分页获取

---
+++ common/common_utils/src/main/java/com/mqd/result/PageInfo.java
@@ -0,0 +1,21 @@
+package com.mqd.result;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class PageInfo {
+    private Long current;
+    private Long size;
+    private Long total;
+    private Long pages;
+
+    public PageInfo(IPage<?> page){
+        this.pages = page.getPages();
+        this.size = page.getSize();
+        this.total = page.getTotal();
+        this.pages = page.getPages();
+    }
+}
--- service/service_edu/src/main/java/com/mqd/eduservice/controller/front/UserCourseController.java
+++ service/service_edu/src/main/java/com/mqd/eduservice/controller/front/UserCourseController.java
@@ -1,7 +1,15 @@
 package com.mqd.eduservice.controller.front;
 
-import com.mqd.eduservice.service.EduCourseDescriptionService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.mqd.eduservice.pojo.EduCourse;
 import com.mqd.eduservice.service.EduCourseService;
+import com.mqd.result.PageInfo;
+import com.mqd.result.Result;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +24,28 @@ public class UserCourseController {
     @Resource
     private EduCourseService courseService;
 
-    @Resource
-    private EduCourseDescriptionService descService;
+    @ApiOperation(value = "获取课程")
+    @GetMapping("/course")
+    public Result getSubject(EduCourse course,Long current, Long size){
+        String subjectParentId = course.getSubjectParentId();
+        String subjectId = course.getSubjectId();
+        QueryWrapper<EduCourse> courseQuery = new QueryWrapper<>();
+        if (current == null || current < 1){
+            current = 1L;
+        }
+        if (size == null || size<10 || size>50){
+            size = 20L;
+        }
+        IPage<EduCourse> page = new Page<>(current,size);
+        //只能获取已发布课程
+        courseQuery.eq("status","Normal");
+        if(StringUtils.hasText(subjectParentId)){
+            courseQuery.eq("subject_parent_id",subjectParentId);
+        }
+        if(StringUtils.hasText(subjectId)){
+            courseQuery.eq("subject_id",subjectId);
+        }
+        page = courseService.page(page,courseQuery);
+        return Result.ok().addData("courseList",page.getRecords()).addData("pageInfo",new PageInfo(page));
+    }
 }
